1351cd118e2fc619e71e8889de9df151c8c2936f
Muna26 <126212421+Muna26@users.noreply.github.com>
2025-08-11 22:03:44 +0700

    Add files via upload

---
+++ controllers/product.js
@@ -0,0 +1,262 @@
+const prisma = require("../config/prisma")
+const { create } = require("./category")
+
+exports.create = async(req,res)=>{
+    try{
+        //code
+        const { title, description, price, quantity, categoryId, images} = req.body
+        //console.log(title, description, price, quantity, images)
+        const product = await prisma.product.create({
+  data: {
+    title: title,
+    description: description,
+    price: parseFloat(price),
+    quantity: parseInt(quantity),
+    category: {
+      connect: {
+        id: parseInt(categoryId)
+      }
+    },
+    Image: {
+      create: images.map((item) => ({
+        asset_id: item.asset_id,
+        public_id: item.public_id,
+        url: item.url,
+        secure_url: item.secure_url
+      }))
+    }
+  }
+});
+        res.send(product)
+    }catch(err){
+        console.log(err)
+        res.status(500).json({ message: "Server error"})
+    }
+}
+
+exports.list = async(req,res)=>{
+    try{
+        //code
+        const { count } = req.params
+        const products = await prisma.product.findMany({
+            take: parseInt(count),
+            orderBy: { createdAt : "desc" },
+            include:{
+                category: true,
+                Image: true
+            }
+        })
+
+
+        res.send(products)
+    }catch(err){
+        console.log(err)
+        res.status(500).json({ message: "Server error"})
+    }
+}
+
+exports.read = async(req,res)=>{
+    try{
+        //code
+        const { id } = req.params
+        const products = await prisma.product.findFirst({
+            where:{
+                id: Number(id)
+            },
+            include:{
+                category: true,
+                Image: true
+            }
+        })
+
+
+        res.send(products)
+    }catch(err){
+        console.log(err)
+        res.status(500).json({ message: "Server error"})
+    }
+}
+
+exports.update = async(req,res)=>{
+    try{
+        const { title, description, price, quantity, categoryId, images} = req.body
+        //console.log(title, description, price, quantity, images)
+
+        await prisma.Image.deleteMany({
+             where: {
+                productId: Number(req.params.id)
+             }
+        })
+
+
+
+
+  const product = await prisma.product.update({
+    where:{
+        id: Number(req.params.findMany)
+    },
+    data: {
+    title: title,
+    description: description,
+    price: parseFloat(price),
+    quantity: parseInt(quantity),
+    category: {
+      connect: {
+        id: parseInt(categoryId)
+      }
+    },
+    Image: {
+      create: images.map((item) => ({
+        asset_id: item.asset_id,
+        public_id: item.public_id,
+        url: item.url,
+        secure_url: item.secure_url
+      }))
+    }
+  }
+});
+        res.send(product)
+    }catch(err){
+        console.log(err)
+        res.status(500).json({ message: "Server error"})
+    }
+}
+
+exports.remove = async(req,res)=>{
+    try{
+        //code
+        const { id } = req.params
+        //ยากก
+
+
+
+
+
+
+        await prisma.product.delete({
+            where:{
+                id: Number(id)
+            }
+        })
+
+
+
+        res.send('Delerted Success')
+    }catch(err){
+        console.log(err)
+        res.status(500).json({ message: "Server error"})
+    }
+}
+
+exports.listby = async(req,res)=>{
+    try{
+        //code //เรียงตามข้อมูลที่ฟร้อนเอ็นส่งมา
+        const { sort, order, limit } = req.body
+        console.log(sort, order, limit)
+        const products = await prisma.product.findMany({
+            take: limit,
+            orderBy: {[sort]: order },
+            include: { category: true}
+        })
+
+
+        res.send(products)
+    }catch(err){
+        console.log(err)
+        res.status(500).json({ message: "Server error"})
+    }
+}
+
+
+const handleQuery = async(req,res,query) => {
+    try{
+    //code
+    const products = await prisma.product.findMany({
+        where:{
+            title: {
+                contains: query,
+            }
+        },
+        include:{
+            category: true,
+            Image: true
+        }
+    })
+    res.send(products)
+    } catch (err) {
+        //err
+        console.log(err)
+        res.status(500).send("Search Error")
+    }
+}
+const handlePrice = async(req,res,priceRange) => {
+    try{
+        const products = await prisma.product.findMany({
+            where:{
+                price:{
+                    gte: priceRange[0],
+                    lte: priceRange[1]
+                }
+            },
+            include:{
+                category: true,
+                Image: true
+            }
+        })
+        res.send(products)
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message: "Search Error"})
+    }
+}
+const handleCategory = async(req,res,categoryId) => {
+    try{
+        const products = await prisma.product.findMany({
+            where:{
+                categoryId:{
+                   in: categoryId.map((id)=> Number(id))
+                }
+            },
+            include:{
+                category: true,
+                Image: true
+            }
+        })
+        res.send(products)
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message: "Search Error"})
+    }
+}
+
+
+exports.searchFilters = async(req,res)=>{
+    try{
+        //code
+        const { query, category, price } = req.body
+
+        if (query) {
+            console.log('query-->', query)
+            await handleQuery(req,res,query)
+        }
+
+        if (category) {
+            console.log('category-->', category)
+            await handleCategory(req,res,category)
+        }
+
+        if (price) {
+            console.log('price-->', price)
+            await handlePrice(req,res,price)
+        }
+
+
+
+
+
+        //res.send('Hello searchFilters Product')
+    }catch(err){
+        console.log(err)
+        res.status(500).json({ message: "Server error"})
+    }
+}
